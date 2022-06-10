eaefca7d1b8af2b9ae60609c37792f2e540828da
Vyacheslav <190103456@gmail.com>
2022-06-11 02:59:01 +0600

    first commit

---
+++ python.py
@@ -0,0 +1,15 @@
+def arithmetic(first, second, answer):
+    if answer == "+":
+        return first + second
+    elif answer == "-":
+        return first - second
+
+    elif answer == "*":
+        return first * second
+    
+    elif answer == "/":
+        return first / second
+
+    else: 
+        return "Undefined"
+
